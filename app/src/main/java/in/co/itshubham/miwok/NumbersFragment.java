fa8add3687e982dbcae85590777b6024679857ce
greatnanu <workforitshubham@gmail.com>
2018-10-29 12:48:51 +0530

    Miwok App complete

--- app/src/main/java/in/co/itshubham/miwok/NumbersFragment.java
+++ app/src/main/java/in/co/itshubham/miwok/NumbersFragment.java
@@ -15,36 +15,30 @@
  */
 package in.co.itshubham.miwok;
 
+
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class ColorsActivity extends AppCompatActivity {
+/**
+ * {@link Fragment} that displays a list of number vocabulary words.
+ */
+public class NumbersFragment extends Fragment {
 
     /** Handles playback of all the sound files */
     private MediaPlayer mMediaPlayer;
 
     /** Handles audio focus when playing a sound file */
     private AudioManager mAudioManager;
-
-    /**
-     * This listener gets triggered when the {@link MediaPlayer} has completed
-     * playing the audio file.
-     */
-    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            // Now that the sound file has finished playing, release the media player resources.
-            releaseMediaPlayer();
-        }
-    };
 
     /**
      * This listener gets triggered whenever the audio focus changes
@@ -75,41 +69,61 @@ public class ColorsActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
+    public NumbersFragment() {
+        // Required empty public constructor
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
 
         // Create and setup the {@link AudioManager} to request audio focus
-        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        mAudioManager = (AudioManager) getActivity().getSystemService(Context.AUDIO_SERVICE);
 
         // Create a list of words
         final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word(R.string.color_red, R.string.miwok_color_red,
-                R.drawable.color_red, R.raw.color_red));
-        words.add(new Word(R.string.color_mustard_yellow, R.string.miwok_color_mustard_yellow,
-                R.drawable.color_mustard_yellow, R.raw.color_mustard_yellow));
-        words.add(new Word(R.string.color_dusty_yellow, R.string.miwok_color_dusty_yellow,
-                R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
-        words.add(new Word(R.string.color_green, R.string.miwok_color_green,
-                R.drawable.color_green, R.raw.color_green));
-        words.add(new Word(R.string.color_brown, R.string.miwok_color_brown,
-                R.drawable.color_brown, R.raw.color_brown));
-        words.add(new Word(R.string.color_gray, R.string.miwok_color_gray,
-                R.drawable.color_gray, R.raw.color_gray));
-        words.add(new Word(R.string.color_black, R.string.miwok_color_black,
-                R.drawable.color_black, R.raw.color_black));
-        words.add(new Word(R.string.color_white, R.string.miwok_color_white,
-                R.drawable.color_white, R.raw.color_white));
+        words.add(new Word(R.string.number_one, R.string.miwok_number_one,
+                R.drawable.number_one, R.raw.number_one));
+        words.add(new Word(R.string.number_two, R.string.miwok_number_two,
+                R.drawable.number_two, R.raw.number_two));
+        words.add(new Word(R.string.number_three, R.string.miwok_number_three,
+                R.drawable.number_three, R.raw.number_three));
+        words.add(new Word(R.string.number_four, R.string.miwok_number_four,
+                R.drawable.number_four, R.raw.number_four));
+        words.add(new Word(R.string.number_five, R.string.miwok_number_five,
+                R.drawable.number_five, R.raw.number_five));
+        words.add(new Word(R.string.number_six, R.string.miwok_number_six,
+                R.drawable.number_six, R.raw.number_six));
+        words.add(new Word(R.string.number_seven, R.string.miwok_number_seven,
+                R.drawable.number_seven, R.raw.number_seven));
+        words.add(new Word(R.string.number_eight, R.string.miwok_number_eight,
+                R.drawable.number_eight, R.raw.number_eight));
+        words.add(new Word(R.string.number_nine, R.string.miwok_number_nine,
+                R.drawable.number_nine, R.raw.number_nine));
+        words.add(new Word(R.string.number_ten, R.string.miwok_number_ten,
+                R.drawable.number_ten, R.raw.number_ten));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
+        WordAdapter adapter = new WordAdapter(getActivity(), words, R.color.category_numbers);
 
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // word_list.xml layout file.
-        ListView listView = (ListView) findViewById(R.id.list);
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
 
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
         // {@link ListView} will display list items for each {@link Word} in the list.
@@ -137,7 +151,7 @@ public class ColorsActivity extends AppCompatActivity {
 
                     // Create and setup the {@link MediaPlayer} for the audio resource associated
                     // with the current word
-                    mMediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudioResourceId());
+                    mMediaPlayer = MediaPlayer.create(getActivity(), word.getAudioResourceId());
 
                     // Start the audio file
                     mMediaPlayer.start();
@@ -148,11 +162,14 @@ public class ColorsActivity extends AppCompatActivity {
                 }
             }
         });
+
+        return rootView;
     }
 
     @Override
-    protected void onStop() {
+    public void onStop() {
         super.onStop();
+
         // When the activity is stopped, release the media player resources because we won't
         // be playing any more sounds.
         releaseMediaPlayer();
